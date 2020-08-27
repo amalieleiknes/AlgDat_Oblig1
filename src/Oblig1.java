58c894009123a65f7da6a2cea777058888100cec
Amalie Leiknes <s340559@oslomet.no>
2020-08-27 13:03:21 +0200

    Oppdatert klassenavn og lagt inn oppgavenr

---
+++ src/Oblig1.java
@@ -0,0 +1,97 @@
+public class Oblig1 {
+
+
+    //Oppgave 1
+
+
+
+
+
+
+
+    // Oppgave 2
+
+
+
+
+
+
+
+    //Oppgave 3
+
+
+
+
+
+
+
+
+    // Oppgave 4
+
+
+
+
+
+
+
+    // Oppgave 5
+
+
+
+
+
+
+
+
+    //Oppgave 6
+
+
+
+
+
+
+    // Oppgave 7
+
+
+
+
+
+
+
+    // Oppgave 8
+
+
+
+
+
+
+
+    // Oppgave 9
+
+
+
+
+
+
+
+    // Oppgave 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/Oppgave1.java
+++
@@ -1,4 +0,0 @@
-public class Oppgave1 {
-
-    // test om det funker
-}
