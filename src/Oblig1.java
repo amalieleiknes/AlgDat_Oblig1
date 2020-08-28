6718fcabdd53ff0942faf10f02e87a8ebbfa28ec
Caroline Jetteberg <s313564@oslomet.no>
2020-08-28 10:55:17 +0200

    Revert "Oppgave 2 - selve metoden, mangler testing"
    This reverts commit 48e4b9471334628a1f67273baed8e25cdd0c8dda.

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -9,29 +9,13 @@ public class Oblig1 {
 
 
 
-    // Oppgave 2 - Må se litt på om denne kan bli mer effektiv
-    public static int antallUlikeSortert(int [] a){
-        boolean sortert = true;
-        for(int i = 1; i < a.length; i++){
-            if(a[i] < a[i-1]){
-                sortert = false;
-            }
-        }
-        if(!sortert){
-            throw new IllegalStateException("Tabellen er ikke sortert stigende!");
-        }
+    // Oppgave 2
 
-        int antallUlike = 1;
-        if(a.length == 0){
-            antallUlike = 0;
-        }
-        for(int i = 1; i < a.length; i++){
-            if(a[i] != a[i-1]){
-                antallUlike++;
-            }
-        }
-        return antallUlike;
-    }
+
+
+
+
+
 
     //Oppgave 3
 
